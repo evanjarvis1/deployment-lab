bdad99844a158cf6de008cb27207b7543dd6f2c7
Evan <evanlegee@gmail.com>
2023-04-06 15:04:52 -0600

    added rollbar support

--- server.js
+++ server.js
@@ -4,4 +4,13 @@ const app = express()
 
 app.use(express.static(`${__dirname}/public`))
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '093c6fd6a12844cfa6011897d26a8fd1',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
+
 app.listen(4000, () => console.log(`server running on 4000`))
